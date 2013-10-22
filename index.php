79655051aab370369c049e9f899472d64956888e
Alexander Shangin <5kr1p7@gmail.com>
2013-10-22 16:50:59 +0400

    Few fixes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$error = false;
 // RESTRICT ACCESS ---------------------------------------------------------------------------------
 $ip = $_SERVER['REMOTE_ADDR'];
 $allowed = preg_match("/^192\.168\.1\..*$/", $ip);
@@ -40,18 +41,17 @@ if($_POST['MAX_FILE_SIZE'] && $allowed) {
 
 		$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-		echo '<pre>';
 		if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 			$arc_pass = substr( filesize($uploadfile) . preg_replace('/[^0-9]/i', '', md5($uploadfile)), 0, $pass_l );
 			if (Repack($uploadfile, $arc_pass) ) {
-				echo '<div class="success-msg">Файл корректен и был успешно загружен.</div>'."\n";
+				$message = '<div class="success-msg">Файл корректен и был успешно загружен.</div>'."\n";
 			} else {
-				echo '<div class="error-msg">Ошибка при упаковке.</div>'."\n";
+				$error = true;
+				$message = '<div class="error-msg">Ошибка при упаковке.</div>'."\n";
 			}
 		} else {
-			echo "Возможная атака с помощью файловой загрузки!\n";
+			$message = '<div class="error-msg">Возможная атака с помощью файловой загрузки!</div>'."\n";
 		}
-		print "</pre>";
 // --------------------------------------------------------------------------------------------------
 
 // CREATE SECURE LINK -------------------------------------------------------------------------------
@@ -76,6 +76,10 @@ if($_POST['MAX_FILE_SIZE'] && $allowed) {
 </head>
 <body>
 	<div id="container">
+		<div id="message" style="visibility: <?php echo $message ? 'visible' : 'hidden'; ?>">
+			<?php echo $message; ?>
+		</div>
+		
 		<div id="logo">
 			<a href="/sec/"><img src="sec_lock.png" /></a>
 			<h1>Генератор защищенных ссылок</h1>
@@ -96,19 +100,23 @@ if($_POST['MAX_FILE_SIZE'] && $allowed) {
 				<label for="exp">Время жизни файла (дней): </label><input maxlength="3" size="5" id="exp" name="exp" class="solid" value="1"><br><br>
 				<input id="btn-create" type="submit" value="Создать ссылку" />
 			</form>
+			<div id="links">
 <?php
 		// PRINT SECURE LINK AND EXPIRE DATE ---------------------------------------------------------------
-		if ($md5 && $expire) {
+		if ($md5 && $expire & !error) {
 			$sec_link = 'https://f.medkirov.ru'.$path.'?st='.$md5.'&e='.$expire;
 
 			echo '<input onclick="this.select();" class="solid" readonly="readonly" style="width:100%" value="'.$sec_link.'"><br>'."\n";
 			echo '<br><center><a target="_blank" href="' . $sec_link . '"\">Открыть защищенную ссылку</a></center><br>'."\n";
 			echo '<div id="exp-date">Ссылка будет работать до: <b>'.date('d.m.Y H:i:s',$expire+14400).'</b></div>'."<br>\n";
-			echo '<div id="pass">Пароль на архив: <b>' . $arc_pass . "</b><br>\n";
+			echo '<div id="pass">Пароль на архив: <b>' . $arc_pass . "</b></div>\n";
 }
 // -------------------------------------------------------------------------------------------------
 ?>
+			</div>
 		</div>
+
+
 	</div>
 </body>
 </html>
